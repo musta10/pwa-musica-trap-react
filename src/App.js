e467e8d85343776c3f22780b29dd6493d7f8ab48
musta10 <mustafaenfadal@gmail.com>
2021-03-26 00:54:34 +0100

    stylelogo

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import React from 'react';
+import Header from "./components/Header"
 
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-      </header>
-    </div>
+   <Header />
   );
 }
 
