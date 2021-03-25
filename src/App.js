29c9342d0ccbd044cfa89a15adabc6bad8b764de
musta10 <mustafaenfadal@gmail.com>
2021-03-26 00:58:42 +0100

    header-color

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-import React from 'react';
-import Header from "./components/Header"
-
+import React from "react";
+import Header from "./components/Header";
+import { BrowserRouter, Switch } from "react-router-dom";
 
 function App() {
   return (
-   <Header />
+    <BrowserRouter>
+      <Switch>
+        <Header />
+      </Switch>
+    </BrowserRouter>
   );
 }
 
